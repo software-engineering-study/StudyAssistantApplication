79e40cd5c58d1ff426e7daa4d6bc50df63170aa3
kurizzing <lkukulza@gmail.com>
2021-12-13 22:17:06 +0900

    MainAcitivity에서 onActivityResult 동작시 resultCode와 상관없이 학습계획 리스트를 초기화하도록 함

--- app/src/main/java/com/se/studyassistantapplication/MainActivity.java
+++ app/src/main/java/com/se/studyassistantapplication/MainActivity.java
@@ -132,9 +132,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_OK){
-            showStudyPlanList(selected_date);
-        }
+        showStudyPlanList(selected_date);
     }
 
     /**
